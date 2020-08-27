ab00e8261d75ca333791cc02a6db1556246b2c29
oasis10702 <oasis10702@gmail.com>
2020-08-27 18:29:23 +0900

    initial ts sample

---
+++ annotations/variables.ts
@@ -0,0 +1,57 @@
+// type annotation
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+
+// Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// 1) When to use annotations
+// Function that returns the 'any' type
+const json = "{x: 10, y: 20}";
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); // {x: 10, y: 20}
+
+// 2) When we declare a variable on one line
+// and initalizate it later
+let words = ["red", "green", "blue"];
+let fountWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "green") {
+    fountWord = true;
+  }
+}
+
+// 3) Varibale whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[0] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
