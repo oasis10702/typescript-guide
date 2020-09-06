946ed222218d6894db0e1128727a23b476c2a739
oasis10702 <oasis10702@gmail.com>
2020-09-06 21:30:35 +0900

    add functions ts guide

---
+++ annotations/functions.ts
@@ -0,0 +1,37 @@
+// use return annnotation because sometimes we may forget return values.
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+// ts can do type inference in return value
+const subtract = (a: number, b: number) => {
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function(a: number, b: number): number {
+  return a * b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): void => {
+  if (!message) {
+    throw new Error(message);
+  }
+};
+
+const forecast = {
+  date: new Date(),
+  weather: 'sunny'
+};
+
+const logWeather = ({ date, weather }: { date: Date; weather: string }): void => {
+  console.log(date);
+  console.log(weather);
+};
