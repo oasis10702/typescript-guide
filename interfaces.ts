33165f22f69573ab813344112dedf566f61315ad
oasis10702 <oasis10702@gmail.com>
2020-09-11 02:00:54 +0900

    add interface ts guide

---
+++ interfaces.ts
@@ -0,0 +1,17 @@
+const oldCivic = {
+  name: "civic",
+  year: 2000,
+  broken: true,
+};
+
+const printVehicle = (vehicle: {
+  name: string;
+  year: number;
+  broken: boolean;
+}): void => {
+  console.log(`Name: ${vehicle.name}`);
+  console.log(`Year: ${vehicle.yaer}`);
+  console.log(`Broken: ${vehicle.broken}`);
+};
+
+printVehicle(oldCivic);
