766a2941a35bfde3751e26c7788d83ae7ca26ae0
oasis10702 <oasis10702@gmail.com>
2021-01-08 19:11:23 +0900

    add Generic Constraints

--- generics.ts
+++ generics.ts
@@ -44,3 +44,30 @@ function printAnything<T>(arr: T[]): void {
 }
 
 printAnything<string>(['a', 'b', 'c']);
+
+// Generic Constraints
+
+class Car {
+  print() {
+    console.log('I am a Car');
+  }
+}
+
+class House {
+  print() {
+    console.log('I am a House');
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHousesOrCars<House>([new House(), new House()]);
+printHousesOrCars<Car>([new Car(), new Car()]);
