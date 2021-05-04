def9fa38359b4f224f94635fa719a077411faeb7
oasis10702 <oasis10702@gmail.com>
2021-05-04 21:15:05 +0900

    property desc

--- decorators.ts
+++ decorators.ts
@@ -1,17 +1,18 @@
 class Boat {
   color: string = 'red';
 
-  @testDecorator
   get formattedColor(): string {
     return `This boats color is ${this.color}`;
   }
 
+  @logError
   pilot(): void {
+    throw new Error();
     console.log('swish');
   }
 }
 
-function testDecorator(target: any, key: string): void {
+function logError(target: any, key: string, desc: PropertyDescriptor): void {
   console.log('Target:', target);
   console.log('Key:', key);
 }
