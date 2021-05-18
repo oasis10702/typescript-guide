9c1de08a51f80220b73c53437beac107992136cb
oasis10702 <oasis10702@gmail.com>
2021-05-18 19:43:07 +0900

    print metadata

--- metadata.ts
+++ metadata.ts
@@ -19,18 +19,25 @@ import 'reflect-metadata';
 // const note = Reflect.getMetadata('note', plane, 'color');
 // console.log(note);
 
+@printMetadata
 class Plane {
   color: string = 'red';
 
-  @markFunction
+  @markFunction('HI THERE')
   fly(): void {
     console.log('vrrrrrr');
   }
 }
 
-function markFunction(target: Plane, key: string) {
-  Reflect.defineMetadata('secret', 123, target, key);
+function markFunction(secretInfo: string) {
+  return function(target: Plane, key: string) {
+    Reflect.defineMetadata('secret', secretInfo, target, key);
+  };
 }
 
-const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly');
-console.log(secret);
+function printMetadata(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const secret = Reflect.getMetadata('secret', target.prototype, key);
+    console.log(secret);
+  }
+}
